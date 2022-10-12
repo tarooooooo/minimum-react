5073468a822775570cd66840b70624a4d05c2aaa
tarooooooo <ryutaromaeda83@gmail.com>
2022-10-12 13:47:19 +0900

    code generator追加・各query & mutationを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,43 @@
-import { ItemsQuery } from "./graphql/generated";
+import {
+  useItemsQuery,
+  useCreateItemMutation,
+  useDeleteItemMutation,
+  useUpdateItemMutation,
+} from "./graphql/generated";
+import {useState} from "react";
 
 function App() {
   const { data: { items = [] } = {} } = useItemsQuery();
+  const [createItem] = useCreateItemMutation({ refetchQueries: ["items"] });
+  const [name, setName] = useState("");
+  const [deleteItem] = useDeleteItemMutation({ refetchQueries: ["items"] });
+  const [updateItem] = useUpdateItemMutation();
 
   return (
     <div>
-      {items.map((item: Item) => (
-        <div key={item.id}>{item.name} : {item.price}</div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          createItem({ variables: { params: { name: name } } });
+          setName("");
+        }}
+      >
+        保存
+      </button>
+      {items.map((item) => (
+        <div key={item.id}>
+          <input
+            value={item.name || ""}
+            onChange={(e) =>
+              updateItem({
+                variables: { id: item.id, params: { name: e.target.value } },
+              })
+            }
+          />
+          <button onClick={() => deleteItem({ variables: { id: item.id } })}>
+          削除
+          </button>
+        </div>
       ))}
     </div>
   );
--- frontend/src/graphql/mutation/deleteItem.ts
+++ frontend/src/graphql/mutation/deleteItem.ts
@@ -1,11 +1,9 @@
 import { gql } from "@apollo/client";
 
 export default gql`
-  query items {
-    items {
+  mutation deleteItem($id: ID!) {
+    deleteItem(input: { id: $id }) {
       id
-      name
-      price
     }
   }
 `;
