642a62e93c6633786b166bfdd1c8b59c0a675860
tarooooooo <ryutaromaeda83@gmail.com>
2022-10-16 15:03:13 +0900

    add: chakra ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   useUpdateItemMutation,
 } from "./graphql/generated";
 import {useState} from "react";
+import {ChakraProvider, Button} from "@chakra-ui/react"
 
 function App() {
   const { data: { items = [] } = {} } = useItemsQuery();
@@ -14,32 +15,34 @@ function App() {
   const [updateItem] = useUpdateItemMutation();
 
   return (
-    <div>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          createItem({ variables: { params: { name: name } } });
-          setName("");
-        }}
-      >
-        保存
-      </button>
-      {items.map((item) => (
-        <div key={item.id}>
-          <input
-            value={item.name || ""}
-            onChange={(e) =>
-              updateItem({
-                variables: { id: item.id, params: { name: e.target.value } },
-              })
-            }
-          />
-          <button onClick={() => deleteItem({ variables: { id: item.id } })}>
-          削除
-          </button>
-        </div>
-      ))}
-    </div>
+    <ChakraProvider>
+      <div>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button
+          onClick={() => {
+            createItem({ variables: { params: { name: name } } });
+            setName("");
+          }}
+        >
+          保存
+        </button>
+        {items.map((item) => (
+          <div key={item.id}>
+            <input
+              value={item.name || ""}
+              onChange={(e) =>
+                updateItem({
+                  variables: { id: item.id, params: { name: e.target.value } },
+                })
+              }
+            />
+            <button onClick={() => deleteItem({ variables: { id: item.id } })}>
+            削除
+            </button>
+          </div>
+        ))}
+      </div>
+    </ChakraProvider>
   );
 }
 
