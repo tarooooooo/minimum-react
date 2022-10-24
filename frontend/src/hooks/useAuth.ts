685005e1d20f688b45a6cc892175881da2eb258c
tarooooooo <ryutaromaeda83@gmail.com>
2022-10-24 20:50:08 +0900

    メッセージ & アイテム一覧ページ追加

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
-  
+  const { showMessage } = useMessage();
+
   const navigate = useNavigate();
 
   const [loading, setLoading] = useState(false);
@@ -16,12 +18,13 @@ export const useAuth = () => {
     // task: 一旦fake_dataを利用。後ほどgraphql経由でバックエンドのデータを利用するように修正
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if(res.data) {
+        showMessage({title: "ログインしました", status: "success"});
         navigate('home');
       } else {
-        alert("ユーザーが見つかりません")
+        showMessage({title: "ユーザーが見つかりません", status: "error"});
       }
     })
-    .catch(() => alert("ログインできません"))
+    .catch(() => showMessage({title: "ログインできません", status: "error"}))
     .finally(() => setLoading(false));
   }, 
   [navigate]
