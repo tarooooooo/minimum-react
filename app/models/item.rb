cfeccaf2ea085a187dca34fef76de76d34d663a5
tarooooooo <75834810+tarooooooo@users.noreply.github.com>
2023-01-09 11:54:10 +0900

    add state to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :category
+
+  scope :active, -> {where(state: :active)}
+  scope :discarded, -> {where(state: :discarded)}
+
+  state_machine :state, initial: :active do
+    state :discarded
+  end
 end
