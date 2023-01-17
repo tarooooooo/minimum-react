ac4a3bcb9a01f89b97d0b268a7e9312f47656ba1
tarooooooo <75834810+tarooooooo@users.noreply.github.com>
2023-01-17 23:44:14 +0900

    不要な差分削除

--- frontend/src/containers/DiscardedItemsPage/index.tsx
+++ frontend/src/containers/DiscardedItemsPage/index.tsx
@@ -3,13 +3,8 @@ import { useDiscardedItemsPageQuery } from "../../graphql/generated"
 
 export const DiscardedItemsContainer: React.VFC = () => {
   const { data } = useDiscardedItemsPageQuery()
-
-  // task: edgesからitemを取り出す処理を、hooksに切り出す
-  // const items = data?.discardedItems?.edges?.map((item) => {
-  //   return (item?.node)
-  // })
   
-  const testItems = data?.discardedItems
+  const items = data?.discardedItems
 
-  return <DiscardedItemsPage items={testItems!} />
+  return <DiscardedItemsPage items={items!} />
 }
