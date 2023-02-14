5df5e480891015518644d2cb4c3a44170877daea
tarooooooo <75834810+tarooooooo@users.noreply.github.com>
2023-02-14 09:54:43 +0900

    一旦保存

--- frontend/src/components/pages/CreateItem.tsx
+++ frontend/src/components/pages/CreateItem.tsx
@@ -7,6 +7,7 @@ import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const CreateItem: VFC = memo(() => {
   const [createItem] = useCreateItemMutation({ refetchQueries: ["items", "categories"] });
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState(1000);
   const { data: {categories = [] } = {} } = useCategoriesQuery();
@@ -22,9 +23,17 @@ export const CreateItem: VFC = memo(() => {
   const onClickCreateItem= () => {
     const categoryItemCount = categoryData?.category.itemCount
     const upperLimit = categoryData?.category.itemStockManagement?.upperLimit
+    
 
     if(categoryItemCount! < upperLimit!) {
       createItem({ variables: { params: { name: name, price: price, categoryId: categoryId } } });
+      if (!file) {
+        return
+      }
+      const formData = new FormData()
+      formData.append("file", file)
+      
+      
       setName("");
       setPrice(1000);
       showMessage({title: "アイテムを追加しました", status: "success"});
@@ -36,6 +45,25 @@ export const CreateItem: VFC = memo(() => {
 
   const categorSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCategoryId(e.target.value)
+  }
+
+  const [file, setFile] = useState<File | null>(null)
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files && files[0]) {
+      setFile(files[0])
+    }
+  }
+
+  const onChangeImage = () => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file!)
+    reader.onload = function() {
+      var image: any
+      image = reader.result;
+      console.log(image)
+    }
   }
 
   return (
@@ -56,6 +84,20 @@ export const CreateItem: VFC = memo(() => {
                 return (<option value={category.id}>{category.name}</option>)
               })}
             </Select>
+            <input
+              name="file"
+              type="file"
+              accept="image/*"
+              onChange={onChangeFile}
+            />
+            <img id="image" />
+
+
+            <Text textAlign="center">
+              <PrimaryButton onClick={onChangeImage}>テスト!</PrimaryButton>
+            </Text>
+
+   
             <Text textAlign="center">
               <PrimaryButton onClick={onClickCreateItem}>Create!</PrimaryButton>
             </Text>
